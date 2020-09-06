ddf8f563f86ae8d88fd7d8bd192f351bbc24ea6a
Steven <steven@exameple.com>
2020-09-06 22:33:05 +0800

    ipywidget version change to taiwan stock list

--- stock_info_service.py
+++ stock_info_service.py
@@ -20,10 +20,10 @@ def save_sp500_stocks_info():
         gics_industry = row.findAll('td')[3].text
         gics_sub_industry = row.findAll('td')[4].text
         
-        tickers.append(ticker.lower().replace(r"\n"," "))
+        tickers.append(ticker.replace(r"\n"," "))
         securities.append(security)
-        gics_industries.append(gics_industry.lower())
-        gics_sub_industries.append(gics_sub_industry.lower())
+        gics_industries.append(gics_industry)
+        gics_sub_industries.append(gics_sub_industry)
 
     stocks_info.append(tickers)
     stocks_info.append(securities)
@@ -35,11 +35,21 @@ def save_sp500_stocks_info():
     stocks_info_df['seclabels'] = 'SP500'
     stocks_info_df['labels'] = stocks_info_df[['tickers','security', 'gics_industry','gics_sub_industry','seclabels']].apply(lambda x: ' '.join(x), axis=1)
 
+    return stocks_info_df
+
+def get_stocks_ipywidgets_options():
+    #df =  save_sp500_stocks_info()
+    df = get_taiwan_stocks_info()
+    options = list(zip( df['labels'],df['tickers']+'.tw' ))
+    options.insert(0,('',''))
+    return  options
+
+def get_stocks_dash_options():
+    df =  save_sp500_stocks_info()
     dictlist = []
-    for index, row in stocks_info_df.iterrows():
+    for index, row in df.iterrows():
         dictlist.append({'value':row['tickers'], 'label':row['labels']})
     return dictlist
-
 
 # Tickers from russell
 # This function will return empty array until the csv file of Russell components is available.
@@ -65,6 +75,40 @@ def save_russell_info():
 
     return dictlist
 
+### Extract lists of the stocks with their tickers
+def get_taiwan_stocks_info():  
+    #resp = requests.get('https://isin.twse.com.tw/isin/C_public.jsp?strMode=2')
+    #soup = BeautifulSoup(resp.text,'lxml')
+    url = "./data/taiwan_stock_list_20200906.html"
+    page = open(url,encoding='ms950')
+    soup = BeautifulSoup(page.read())
+    rows = [row for row in soup.find_all('tr') if len(row.findAll('td'))>5 and row.findAll('td')[5].text=='ESVUFR'  ]
+    stocks_info=[]
+    tickers, securities, gics_industries, gics_sub_industries = [],[],[],[]
+    for row in rows:
+        company = row.findAll('td')[0].text.split()
+        ticker = company[0]
+        security = company[1]
+        gics_industry = row.findAll('td')[3].text
+        gics_sub_industry = row.findAll('td')[4].text
+        
+        tickers.append(ticker.replace(r"\n"," "))
+        securities.append(security)
+        gics_industries.append(gics_industry)
+        gics_sub_industries.append(gics_sub_industry)
+
+    stocks_info.append(tickers)
+    stocks_info.append(securities)
+    stocks_info.append(gics_industries)
+    stocks_info.append(gics_sub_industries)
+
+    stocks_info_df = pd.DataFrame(stocks_info).T
+    stocks_info_df.columns=['tickers','security','gics_industry','gics_sub_industry']
+    stocks_info_df['seclabels'] = 'TWSE'
+    stocks_info_df['labels'] = stocks_info_df[['tickers','security', 'gics_industry','gics_sub_industry','seclabels']].apply(lambda x: ' '.join(x), axis=1)
+
+    return stocks_info_df
+
 
 # self append other stock list
 def save_self_stocks_info():
@@ -88,8 +132,10 @@ def save_self_stocks_info():
 ### Get stock price record from yahoo
 def get_stock_price(ticker,start_date,end_date=dt.now()):
     if (ticker != None):
+        #ticker='2330.tw'
+        #ticker='GOOG'
         return pdr.DataReader(
-            ticker,data_source='yahoo',start=dt(2020,1,1),end=end_date
+            ticker.strip(),data_source='yahoo',start=dt(2020,1,1),end=end_date
         )
     else:
         print('Need stock ticker parameter')
@@ -103,8 +149,12 @@ def plot_stock_chart(ticker,title,stockprice_df):
     plt.show()
 
 ### Draw classic stock K chart
-def plot_stock_k_chart(ticker,title,stockprice_df):
-    quotes = stockprice_df[['Open','High','Low','Close']]
+def plot_stock_k_chart(ticker):
+    title=''
+    if (ticker==None or ticker.strip()==''):
+        return None
+    stockprice_df=get_stock_price(ticker,dt(2020,1,1),end_date=dt.now())  
+    quotes = stockprice_df[['Open','High','Low','Close','Volume']]
     qf = cf.QuantFig(
         quotes,
         title=title,
@@ -121,15 +171,19 @@ def plot_stock_k_chart(ticker,title,stockprice_df):
         color='red'
     )
     qf.add_sma(
-        periods=1,
+        periods=20,
         column='Close',
         name='sma-20',
         str='20日線'
     )
+    #qf.add_volume()
     qf.add_macd()
     qf.add_rsi(periods=14, showbands=False)
-    plyo.iplot(
-        qf.iplot(asFigure=True,    dimensions=(800,600)),
-        filename='qf_img'
-    )
-
+    #print(ticker)
+  #  fig = plyo.iplot(
+  #      qf.iplot(asFigure=True,    dimensions=(800,600)),
+  #      filename='qf_img'
+  #  )
+    #return qf.iplot(asFigure=True,    dimensions=(800,600))
+    #return qf.iplot()
+    return qf.iplot()
